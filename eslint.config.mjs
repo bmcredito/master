761d652c033c8524cf3c3ef20a28bd415335e617
bmcredito <erico.andrade@b3ware.com.br>
2026-09-03 17:26:55 -0300

    fix(platform): make ESLint flat config self-contained

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import nextPlugin from "eslint-config-next";
 
 export default [
-  ...nextPlugin,
   { ignores: [".next/**", "node_modules/**"] },
-  { files: ["**/*.ts", "**/*.tsx"], languageOptions: { parser: tsParser }, plugins: { "@typescript-eslint": tseslint } }
+  { files: ["**/*.ts", "**/*.tsx"], languageOptions: { parser: tsParser }, plugins: { "@typescript-eslint": tseslint }, rules: { "@typescript-eslint/no-unused-vars": "error" } }
 ];
